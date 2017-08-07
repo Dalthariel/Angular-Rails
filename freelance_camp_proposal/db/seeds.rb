03e36b6544708b595d757b383a62498e132c8b69
Dalthariel <david.hamaker@gmail.com>
2017-08-07 14:51:48 -0600

    created seeds data for proposal microservice rails app

---
+++ freelance_camp_proposal/db/seeds.rb
@@ -0,0 +1,13 @@
+10.times do |proposal|
+	Proposal.create!(
+		customer: "Customer #{proposal}",
+		portfolio_url: string = "http://davidhamaker.com",
+		tools: "Ruby on Rails, Angular2, and PostGreSQL",
+		estimated_hours: (70 + proposal),
+		hourly_rate: 50,
+		weeks_to_complete: 5,
+		client_email: "david.hamaker@gmail.com",
+	)
+end
+
+puts "10 proposals created"
